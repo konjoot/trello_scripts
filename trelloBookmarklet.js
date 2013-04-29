99e909b4a6ceca53231a928195e3145880387ad2
maksimov <maksimov@garant.ru>
2013-04-29 12:04:50 +0400

    Important fix for card resize

--- trelloBookmarklet.js
+++ trelloBookmarklet.js
@@ -4,7 +4,7 @@
 		var window_wrapper_width = screen_width - screen_width/10;
 		var window_main_col_width = window_wrapper_width - 250;
 		$('.window').css('left', (screen_width/20).toString() + 'px');
-		$('.window-wrapper').css('width', window_wrapper_width.toString() + 'px');
+		$('.window').css('width', window_wrapper_width.toString() + 'px');
 		$('.window-main-col').css('width', window_main_col_width.toString() + 'px');
 		$('.icon-lg.icon-close.dark-hover.js-close-window').on('click', function(){
 				$('.window-wrapper').css('width', '100%');
